14936d86f36cb55fbd9f31c4884db66e7a1b08bd
bravotango <seattleriot@hotmail.com>
2021-11-28 20:20:35 -0800

    Deleted routes/html.js & kept in server.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 
 // Import all the routes
-//const htmlRoutes = require('./html.js');
 const notesRoutes = require('./notesRoutes.js');
 
-//router.use(htmlRoutes);
 router.use(notesRoutes);
 
 module.exports = router;
